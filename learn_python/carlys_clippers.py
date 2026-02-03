6bfd72156856f8dacc6e82943e0591d64d2a5195
BrandoKlawrissian <54420282+BrandoKlawrissian@users.noreply.github.com>
2026-02-02 18:43:25 -0600

    Refactor total_price calculation in carlys_clippers.py
    Removed redundant calculation of total_price in a loop and used sum() directly.

--- learn_python/carlys_clippers.py
+++ learn_python/carlys_clippers.py
@@ -5,10 +5,7 @@ prices = [30, 25, 40, 20, 20, 35, 50, 35]
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
-
-for price in prices:
-  total_price = sum(prices)
-print(total_price)
+total_price = sum(prices)
 
 average_price = total_price / len(prices)
 print(average_price)
